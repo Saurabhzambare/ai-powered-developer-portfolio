f9b7877611a269aa4861db025b79c3164b63c6ed
Saurabhzambare <43110882+Saurabhzambare@users.noreply.github.com>
2026-09-06 14:54:21 +0530

    feat(design-system): add reduced motion policy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { MotionConfig } from 'motion/react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
@@ -7,10 +8,12 @@ import { AppRoutes } from './app/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <MotionConfig reducedMotion="user">
+      <ErrorBoundary>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </MotionConfig>
   </StrictMode>,
 )
